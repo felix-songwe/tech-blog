de4253a9fae52d54dc5ff78c17541a1ac2ad3e90
felix-songwe <felixsongwe0@gmail.com>
2024-03-13 10:14:12 -0700

    installed react router DOM. I then added a way to have multiple pages to our App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import Navbar from "./navbar";
 import Home from "./home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   // #3. we create a dynamic value in ths 'const' give it a title and retuen it in the curly brackets.
   return (
-    <div className="App">
-      {/* after we import the navbar, we nest it. It is a self closing tag. */}
-      <Navbar />
-
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        {/* after we import the navbar, we nest it. It is a self closing tag. */}
+        <Navbar />
+        {/* #19. This is were our page content will go when to go to different pages */}
+        <div className="content">
+          <Switch>
+            <Route path="/">   {/* show the home page when user visits an empty path of  "/" */}
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
