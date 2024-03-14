0ffcd9ecf885300bf8766d3de43e940654eb67fc
felix-songwe <felixsongwe0@gmail.com>
2024-03-13 19:48:45 -0700

    Added additional functionality to our form to prevent it from refreshing when we want to submit our data.

--- src/create.js
+++ src/create.js
@@ -4,13 +4,18 @@ const Create = () => {
    const [title, setTitle] = useState("");
    const [body, setBody] = useState("");
    const [author, setAuthor] = useState("mario");
+   // #26. Submit events
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const blog = { title, body, author }
+   }
 
    return (
      <div className="create">
        <h2>Add a new blog</h2>
 
        {/* #25. Input form */}
-       <form>
+       <form onSubmit={ handleSubmit }>
          <label>Blog title</label>
          <input
            type="text"
