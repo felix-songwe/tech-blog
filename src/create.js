28cdbee13bf0e7a6e72c6bd9591c0cdfff8ba90d
felix-songwe <felixsongwe0@gmail.com>
2024-03-13 19:30:19 -0700

    Created an input form in our create file. Styled it in our css file, and added a bit of functionality.

--- src/create.js
+++ src/create.js
@@ -1,8 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
+   const [title, setTitle] = useState("");
+   const [body, setBody] = useState("");
+   const [author, setAuthor] = useState("mario");
+
    return (
-      <div className="create">
-         <h2>Add a new blog</h2>
-      </div>
+     <div className="create">
+       <h2>Add a new blog</h2>
+
+       {/* #25. Input form */}
+       <form>
+         <label>Blog title</label>
+         <input
+           type="text"
+           required
+           value={ title }
+           onChange={(e) => setTitle(e.target.value)}
+         />
+         <label>Blog body</label>
+         <textarea
+           required
+           value={ body }
+           onChange={(e) => setBody(e.target.value)}
+         ></textarea>
+         <label>Blog author</label>
+         <select>
+            {/* Blog author */}
+            value={ author}
+            onChange={(e) => setAuthor(e.target.value)}
+
+           <option value="mario">Mario</option>
+           <option value="yoshi">Yoshi</option>
+         </select>
+       </form>
+       {/* button */}
+       <button>Add blog</button>
+       <p>{ title }</p>
+       <p>{ body }</p>
+       <p>{ author }</p>
+     </div>
    );
 }
  
